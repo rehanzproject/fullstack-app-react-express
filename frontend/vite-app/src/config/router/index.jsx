425c47711cd4e3e6076d8e88123ccbe230598a7e
Rehan Maulana <rehanmaul111@gmail.com>
2023-08-12 22:26:39 +0700

    chore: remove unused component

--- frontend/vite-app/src/config/router/index.jsx
+++ frontend/vite-app/src/config/router/index.jsx
@@ -4,7 +4,6 @@ import LandingPage from "../../components/views/LandingPage.view";
 import Login from "../../components/views/LoginPage.view";
 import HomePage from "../../components/views/HomePage.view";
 import AdminTemplate from "../../components/templates/Admin.template";
-import AdminPage from "../../components/views/Admin/AdminPage.view";
 import AdminAnalystic from "../../components/views/Admin/AdminAnalystic.view";
 import AdminCalendar from "../../components/views/Admin/AdminCalendar.view";
 import AdminProfile from "../../components/views/Admin/AdminProfile.view";
@@ -18,7 +17,6 @@ const RootRouter = () => {
         <Route path="register" element={<Register />} />
         <Route path="home" element={<HomePage />} />
         <Route element={<AdminTemplate />}>
-          <Route path="/admin" element={<AdminPage />} />
           <Route path="/admin/analystic" element={<AdminAnalystic />} />
           <Route path="/admin/calendar" element={<AdminCalendar />} />
           <Route path="/admin/profile" element={<AdminProfile />} />
