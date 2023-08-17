535128e089f5d110afe0f93ccc2f2ca8e81684f3
Rehan Maulana <rehanmaul111@gmail.com>
2023-08-17 19:09:10 +0700

    test cors

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,11 @@ import express from "express";
 import ProductRouter from "./ProductRouter.js";
 import UserRouter from "./UserRouter.js";
 const router = express.Router();
+router.use((req, res , next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 router.use("/product", ProductRouter)
 router.use("/user", UserRouter)
 export default router;
