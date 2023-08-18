32df7ac6fe1fb7ae2070dffc95a59940f3dd7a8c
Rehan Maulana <rehanmaul111@gmail.com>
2023-08-18 18:40:48 +0700

    dss

--- backend/index.js
+++ backend/index.js
@@ -26,11 +26,12 @@ try {
   console.error(error);
 }
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://diverse-restaurant.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Credentials", true)
   next();
 });
 app.use(
